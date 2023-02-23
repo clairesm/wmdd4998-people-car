eef51080e436167d52480a9b57e4f393bf3d2592
Claire Simbulan <elleclairesimbulan@gmail.com>
2023-02-22 16:48:51 -0800

    frontend remove a car

--- client/src/components/buttons/RemoveCar.js
+++ client/src/components/buttons/RemoveCar.js
@@ -1,9 +1,24 @@
 import { DeleteOutlined } from '@ant-design/icons';
 import { useMutation } from '@apollo/client';
-import { REMOVE_CAR } from '../../queries';
+import filter from 'lodash.filter';
+import { GET_CARS, REMOVE_CAR } from '../../queries';
 
 const RemoveCar = ({ id }) => {
-  const [removeCar] = useMutation(REMOVE_CAR);
+  const [removeCar] = useMutation(REMOVE_CAR, {
+    update(cache, { data: { removeCar } }) {
+      const { cars } = cache.readQuery({
+        query: GET_CARS,
+      });
+      cache.writeQuery({
+        query: GET_CARS,
+        data: {
+          cars: filter(cars, (c) => {
+            return c.id !== removeCar.id;
+          }),
+        },
+      });
+    },
+  });
   const handleButtonClick = () => {
     let result = window.confirm(
       'Are you sure you want to delete this car?'
