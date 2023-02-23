d31205b7039308c4a94404414cfb995e13d5b0f1
Claire Simbulan <elleclairesimbulan@gmail.com>
2023-02-23 02:38:47 -0800

    AddCar ternary condition

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -3,13 +3,20 @@ import AddCar from '../forms/AddCar';
 import AddPerson from '../forms/AddPerson';
 import Title from '../layout/Title';
 import People from '../lists/People';
+import { GET_PEOPLE } from '../../queries';
+import { useQuery } from '@apollo/client';
 
 const Home = () => {
+  const { loading, error, data } = useQuery(GET_PEOPLE);
+
+  const hasPeople =
+    !loading && !error && data.people.length > 0;
+
   return (
     <div>
       <Title />
       <AddPerson />
-      <AddCar />
+      {hasPeople && <AddCar />}
       <People />
     </div>
   );
